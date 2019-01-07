d4bf076777fba18e8879aa1b328a3e4a9136a0f2
lihao <lihao_100@boe.com.cn>
2019-01-07 15:15:20 +0800

    添加测试接口

--- src/main/java/com/haoli/demo/controller/EmailController.java
+++ src/main/java/com/haoli/demo/controller/EmailController.java
@@ -33,19 +33,29 @@ public class EmailController {
 		MailConfig mailConfig = emailDao.getMailConfig(qparams);
 		Map<String, Object> params = new HashMap<String, Object>();
 		String subject = "李昊测试邮件";
-		String hotelEmail = "http://boe-ssc-object.oss-cn-beijing.aliyuncs.com/pdf/zhusu.pdf";
+		String attachment = "http://boe-ssc-object.oss-cn-beijing.aliyuncs.com/pdf/zhusu.pdf";
 		String img = "https://www.baidu.com/img/bd_logo1.png";
 		params.put("cid1", "qrcode");
 		params.put("cid2", "img");
 		String content = velocityUtil.getText("templates/mail/demoMailTemplate.vm", params);
 		String[] toList = {"lihao_100@boe.com.cn"};
-
 		try{
 			EmailUtil emailUtil = new EmailUtil(mailConfig);
-			emailUtil.sendEmail(toList, subject, content, null, new String[]{"hotel","img"}, new String[]{hotelEmail,img});
+			emailUtil.sendEmail(toList, subject, content, null, new String[]{"attachment","img"}, new String[]{attachment,img});
 			return new JsonResponse<String>("true");
 		} catch(Exception e) {
 			return new JsonResponse<String>("false");
 		}
+	}
+	
+	@GetMapping("/demo/testConnect")
+	public JsonResponse<Boolean> testConnect() throws Exception{
+		Long userId = 2L;
+		Map<String, Object> qparams = new HashMap<String, Object>();
+		qparams.put("userId", userId);
+		MailConfig mailConfig = emailDao.getMailConfig(qparams);
+		EmailUtil emailUtil = new EmailUtil(mailConfig);
+		boolean flag = emailUtil.connect();
+		return new JsonResponse<Boolean>(flag);
 	}
 }
