74e0c2c1fbcb060b18ab31958e9d30f8f315c3ee
lihao <lihao_100@boe.com.cn>
2019-01-25 15:23:20 +0800

    版本升级

--- src/main/java/com/haoli/HaoliDemo.java
+++ src/main/java/com/haoli/HaoliDemo.java
@@ -3,8 +3,6 @@ package com.haoli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
